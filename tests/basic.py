c501e888453d42b8d9eb78d0b99fe7aa73d77600
Pranav Kant <pranav913@gmail.com>
2014-04-15 18:15:29 +0530

    Added infrastructure for tests

---
+++ tests/basic.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+from testutil import *
+
+from gi.repository import Gio, GLib
+
+import os, sys
+import pyatspi
+from dogtail import tree
+from dogtail import utils
+from dogtail.procedural import *
+
+init()
+try:
+    app = start()
+
+finally:
+    fini()
